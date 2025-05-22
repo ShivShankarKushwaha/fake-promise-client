a86c4557b2a920e0f4b6dfe22558ddb961e20a62
ShivShankarKushwaha <shivshankarkushwaha0000@gmail.com>
2025-05-22 11:03:18 +0530

    add: inlang

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
+import { paraglideVitePlugin } from '@inlang/paraglide-js';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [paraglideVitePlugin({ project: './project.inlang', outdir: './src/paraglide' }), sveltekit()],
 	test: {
 		environment: 'jsdom',
 		globals: true,
